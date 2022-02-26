169afdd47d9cf416a297e2083905364478c36080
Timothy Samraj <tsam0005@student.monash.edu>
2022-02-26 11:20:19 +1100

    Initial commit

---
+++ lib/unihack-backend-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class UnihackBackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'UnihackBackendQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
